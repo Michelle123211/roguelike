03b5f0a73580fed1134c014b761e3197e515a9dc
Michelle123211 <M.stolova@seznam.cz>
2021-05-18 13:20:24 +0200

    First experiment with custom tileset

--- src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/RenderArea.kt
+++ src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/RenderArea.kt
@@ -4,13 +4,14 @@ import cz.cuni.gamedev.nail123.roguelike.blocks.Wall
 import cz.cuni.gamedev.nail123.roguelike.extensions.asNullable
 import cz.cuni.gamedev.nail123.roguelike.tiles.GameTiles
 import cz.cuni.gamedev.nail123.roguelike.world.Area
+import cz.cuni.gamedev.nail123.roguelike.world.worlds.WaveFunctionCollapsedWorld
 import org.hexworks.zircon.internal.tileset.SwingTilesetLoader
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
 fun main() {
-    val game = Game()
+    val game = Game(WaveFunctionCollapsedWorld())
     game.world.currentArea.toPNG("out/render.png")
 }
 
--- src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/tiles/GameTiles.kt
+++ src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/tiles/GameTiles.kt
@@ -8,7 +8,7 @@ import org.hexworks.zircon.api.resource.TilesetResource
 
 object GameTiles {
     val defaultCharTileset = CP437TilesetResources.rogueYun16x16()
-    val defaultGraphicalTileset = TilesetResources.kenney16x16
+    val defaultGraphicalTileset = TilesetResources.stolova16x16
 
     val EMPTY: Tile = Tile.empty()
 
@@ -18,16 +18,16 @@ object GameTiles {
     // Wall tile is replaced by autotiling in Wall.kt
     val WALL = characterTile('#', GameColors.WALL_FOREGROUND, GameColors.WALL_BACKGROUND)
 
-    val PLAYER = graphicalTile("Adventurer")
+    val PLAYER = graphicalTile("Female")
     val CLOSED_DOOR = graphicalTile("Door closed")
     val OPEN_DOOR = graphicalTile("Door open")
     val STAIRS_DOWN = graphicalTile("Stairs down")
     val STAIRS_UP = graphicalTile("Stairs up")
     val BLACK = characterTile(' ', GameColors.BLACK, GameColors.BLACK)
 
-    val RAT = graphicalTile("Rat")
+    val RAT = graphicalTile("Animal 1")
 
-    val SWORD = graphicalTile("Sword")
+    val SWORD = graphicalTile("Magic wand")
 
     // Autotiling tries to find a tile by whether similar tiles neighbor in some directions
     // It iterates through a list of Directional flags (which must be fulfilled) to tiles that should be used
@@ -35,25 +35,26 @@ object GameTiles {
     private val all8 = Direction.eightDirections.sumBy { it.flag }
     val wallTiling = Autotiling(
             // Default
-            graphicalTile("Wall thick W E"),
+            graphicalTile("Wall 2"),
             // Walls all around
-            all8 to graphicalTile("Black"),
+            all8 to graphicalTile("Floor"),
             // Walls everywhere except one corner
-            all8 - Direction.NORTH_WEST.flag to graphicalTile("Wall thick N W"),
-            all8 - Direction.NORTH_EAST.flag to graphicalTile("Wall thick N E"),
-            all8 - Direction.SOUTH_WEST.flag to graphicalTile("Wall thick S W"),
-            all8 - Direction.SOUTH_EAST.flag to graphicalTile("Wall thick S E"),
+            all8 - Direction.NORTH_WEST.flag to graphicalTile("Wall 7"),
+            all8 - Direction.NORTH_EAST.flag to graphicalTile("Wall 6"),
+            all8 - Direction.SOUTH_WEST.flag to graphicalTile("Wall 3"),
+            all8 - Direction.SOUTH_EAST.flag to graphicalTile("Wall 1"),
             // Lines
-            Direction.NORTH + Direction.SOUTH to graphicalTile("Wall thick N S"),
-            Direction.WEST + Direction.EAST to graphicalTile("Wall thick W E"),
+            // TODO: Differenciate between left and right Wall 4, up and bottom Wall 2
+            Direction.NORTH + Direction.SOUTH to graphicalTile("Wall 4"),
+            Direction.WEST + Direction.EAST to graphicalTile("Wall 2"),
             // Corners
-            Direction.NORTH + Direction.EAST to graphicalTile("Wall thick N E"),
-            Direction.NORTH + Direction.WEST to graphicalTile("Wall thick N W"),
-            Direction.SOUTH + Direction.EAST to graphicalTile("Wall thick S E"),
-            Direction.SOUTH + Direction.WEST to graphicalTile("Wall thick S W"),
+            Direction.NORTH + Direction.EAST to graphicalTile("Wall 6"),
+            Direction.NORTH + Direction.WEST to graphicalTile("Wall 7"),
+            Direction.SOUTH + Direction.EAST to graphicalTile("Wall 1"),
+            Direction.SOUTH + Direction.WEST to graphicalTile("Wall 3"),
             // Single adjacent (horizontal ones fallback to default)
-            Direction.NORTH.flag to graphicalTile("Wall thick N S"),
-            Direction.SOUTH.flag to graphicalTile("Wall thick N S")
+            Direction.NORTH.flag to graphicalTile("Wall 4"),
+            Direction.SOUTH.flag to graphicalTile("Wall 4")
     )
 
     fun characterTile(char: Char,
